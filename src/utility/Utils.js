163db7bca2746046b13a064111152f9f4974bf08
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-07-21 16:49:51 +0330

    update utils.js for authorization

--- src/utility/Utils.js
+++ src/utility/Utils.js
@@ -52,7 +52,7 @@ export const formatDateToMonthShort = (value, toTimeForCurrentDay = true) => {
  ** This is completely up to you and how you want to store the token in your frontend application
  *  ? e.g. If you are using cookies to store the application please update this function
  */
-export const isUserLoggedIn = () => true // Remember to change this later on...
+export const isUserLoggedIn = () => localStorage.getItem("userData")
 export const getUserData = () => JSON.parse(localStorage.getItem("userData"))
 
 /**
