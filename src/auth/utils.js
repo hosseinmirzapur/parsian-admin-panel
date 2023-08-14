36d27b61b973dfeb4f3dd8970d8a378cf12d6603
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-08-15 00:10:24 +0330

    updated a function

--- src/auth/utils.js
+++ src/auth/utils.js
@@ -7,7 +7,10 @@ import useJwt from "@src/@core/auth/jwt/useJwt"
  */
 // eslint-disable-next-line arrow-body-style
 export const isUserLoggedIn = () => {
-	return localStorage.getItem("userData") && localStorage.getItem("token")
+	return (
+		localStorage.getItem("token") !== null &&
+		localStorage.getItem("token") !== ""
+	)
 }
 
 export const getUserData = () => JSON.parse(localStorage.getItem("userData"))
