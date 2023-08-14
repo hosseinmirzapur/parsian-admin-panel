1785727a2d33403ecffd9e07cabfe549f2458ab1
Hossein Mirzapur <63141950+hosseinmirzapur@users.noreply.github.com>
2023-08-15 00:42:52 +0330

    updated router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -104,9 +104,6 @@ const Router = () => {
 		} else if (route.meta && route.meta.authRoute && isUserLoggedIn()) {
 			// ** If route has meta and authRole and user is Logged in then redirect user to home page (DefaultRoute)
 			return <Redirect to="/" />
-		} else if (isUserLoggedIn() && !ability.can(action || "read", resource)) {
-			// ** If user is Logged in and doesn't have ability to visit the page redirect the user to Not Authorized
-			return <Redirect to="/misc/not-authorized" />
 		} else {
 			// ** If none of the above render component
 			return <route.component {...props} />
